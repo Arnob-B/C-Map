2ee346a66aacc70a8cc390b948aab661f2ce8303
Somnath Chattaraj <135858837+Somnath-Web-Developer@users.noreply.github.com>
2024-02-06 21:53:51 +0530

    Add Street light and crime to the map

--- mern-openlayers/server.js
+++ mern-openlayers/server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // MongoDB connection
-mongoose.connect(process.env.dburl);
+mongoose.connect('mongodb://127.0.0.1:27017/mernmap');
 
 app.use(express.json());
 
